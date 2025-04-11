7ca96b41b94180a852a93fc1804c737b1b3f2ebe
Rahul Nath <nathrahul563@gmail.com>
2025-04-11 15:49:29 +0530

    feat: add MongoDB connection utility and basic server route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './lib/connectDb';
+
 
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
+// Basic route
 app.get('/', (req, res) => {
     res.send('Hello World!');
-    }
-);
+});
+
+// Start the server
+app.listen(PORT, async () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+    connectDB();
+});
